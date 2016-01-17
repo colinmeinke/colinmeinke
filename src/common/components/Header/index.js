89e8879eee6a32125cbcc4a539b2bd3b53618395
Colin Meinke <hello@colinmeinke.com>
2016-01-17 17:06:16 -0600

    feat(header): basic styling

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -1,27 +1,58 @@
 import React from 'react';
 import { Link } from 'universal-redux-router';
 
+import coreStyles from './core.css';
+
+const navItems = [
+  { name: 'Blog', url: '/' },
+  { name: 'About', url: '/about' },
+  { name: 'Projects', url: '/projects' },
+];
+
 const Header = ({ url }) => (
-  <header>
-    <h1>Colin Meinke</h1>
+  <header className={ coreStyles.header }>
+    <h1 className={ coreStyles.title }>Colin Meinke</h1>
 
-    <p>Making websites in far away places.</p>
+    <p className={ coreStyles.subTitle }>
+      Making websites in far away places.
+    </p>
 
-    <p>Currently learning Spanish in Oaxaca, Mexico.</p>
+    <p className={ coreStyles.activityLocation }>
+      Currently { ' ' }
+      <strong className={ coreStyles.activity }>
+        learning Spanish
+      </strong>
+      { ' ' } in { ' ' }
+      <strong className={ coreStyles.location }>
+        Oaxaca, Mexico
+      </strong>.
+    </p>
 
-    <ul>
-      <li className={ url === '/' ? 'active' : '' }>
-        <Link url="/">Blog</Link>
-      </li>
-      <li className={ url === '/about' ? 'active' : '' }>
-        <Link url="/about">About</Link>
-      </li>
-      <li className={ url === '/projects' ? 'active' : '' }>
-        <Link url="/projects">Projects</Link>
-      </li>
+    <ul className={ coreStyles.nav }>
+      { navItems.map(( item, i ) => (
+        <li
+          className={[
+            coreStyles.navItem,
+            ( url === item.url ? coreStyles.navActive : null ),
+          ].join( ' ' )}
+          key={ i }
+        >
+          <Link
+            className={ coreStyles.navLink }
+            url={ item.url }
+          >
+            { item.name }
+          </Link>
+        </li>
+      ))}
     </ul>
 
-    <Link url="/work-with-me">Work with me</Link>
+    <Link
+      className={ coreStyles.button }
+      url="/work-with-me"
+    >
+      Work with me
+    </Link>
   </header>
 );
 
