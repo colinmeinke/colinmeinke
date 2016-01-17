27a0f64bab2e548980709020b4030026734c59bd
Colin Meinke <hello@colinmeinke.com>
2016-01-17 17:18:37 -0600

    feat(footer): basic styling

--- src/common/components/Footer/index.js
+++ src/common/components/Footer/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Link } from 'universal-redux-router';
 
+import coreStyles from './core.css';
+
 const Footer = () => (
-  <footer>
-    <p>I'm taking on new projects in 2016.</p>
-    <Link url="/work-with-me">Discuss working with me remotely</Link>
+  <footer className={ coreStyles.footer }>
+    <p className={ coreStyles.title }>
+      I'm taking on new projects in 2016.
+    </p>
+
+    <Link
+       className={ coreStyles.button }
+       url="/work-with-me"
+    >
+      Discuss working with me remotely
+    </Link>
   </footer>
 );
 
