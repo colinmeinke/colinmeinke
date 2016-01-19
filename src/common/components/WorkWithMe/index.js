f43388aa61fec53905c18725a21cbbf098952445
Colin Meinke <hello@colinmeinke.com>
2016-01-18 18:39:02 -0600

    refactor(copy): add what i do to work with me copy

--- src/common/components/WorkWithMe/index.js
+++ src/common/components/WorkWithMe/index.js
@@ -13,29 +13,97 @@ const WorkWithMe = () => (
       <section className={ baseStyles.containter }>
         <h2 className={ baseStyles.title }>Work with me</h2>
 
-        <p>I'm taking on new projects in 2016.</p>
+        <section className={ baseStyles.section }>
+          <p>
+            I have over 10 years of experience building
+            full-stack websites, both working full-time within
+            start-ups, and freelance for non-profits.
+          </p>
 
-        <p>
-          If you need somebody to take your project from
-          start to finish, or anywhere in between, I've got
-          you covered.
-        </p>
+          <p>
+            <a
+              className={ baseStyles.link }
+              href="https://uk.linkedin.com/in/colinmeinke"
+            >
+              Take a look at my work experience.
+            </a>
+          </p>
+        </section>
 
-        <p>
-          <strong>
-            Drop me an email to discuss your project and
-            get things rolling.
-          </strong>
-        </p>
+        <section className={ baseStyles.section }>
+          <h2>What I do</h2>
 
-        <p>
-          <a
-            className={ baseStyles.link }
-            href="mailto:hello@colinmeinke.com"
-          >
-            hello@colinmeinke.com
-          </a>
-        </p>
+          <ul className={ baseStyles.list }>
+            <li>
+              <h3>Development</h3>
+              <p>
+                Javascript moves at the speed of light, and
+                I love learning, so it's a great match for
+                me. My current stack includes React, Redux,
+                NodeJS, Express, Babel, CSS modules, PostCSS
+                and Webpack.
+              </p>
+              <p>
+                I pride myself on being that rare mix of
+                both creative and technical. I love styling
+                just as much as I do architecting
+                maintainable code.
+              </p>
+            </li>
+
+            <li>
+              <h3>Experience design</h3>
+              <p>
+                I believe design is about context. Who's
+                using your website? Where are they? What
+                are they doing? Design isn't just about
+                a <em>user's </em> journey from A to B
+                through your website, but about a <em>
+                person's</em> ongoing experience with your
+                product in real, imperfect and changing
+                environments.
+              </p>
+            </li>
+
+            <li>
+              <h3>Everywhere</h3>
+              <p>
+                I want the experiences I create to work
+                well for everyone, wherever they are,
+                regardless of their device or connection.
+                Progressive enhancement, accessibility,
+                responsive web design and performance are
+                all key parts of this philosophy.
+              </p>
+            </li>
+          </ul>
+        </section>
+
+        <section className={ baseStyles.section }>
+          <p>I'm taking on new projects in 2016.</p>
+
+          <p>
+            If you need somebody to take your project from
+            start to finish, or anywhere in between, I've got
+            you covered.
+          </p>
+
+          <p>
+            <strong>
+              Drop me an email to discuss your project and
+              get things rolling.
+            </strong>
+          </p>
+
+          <p>
+            <a
+              className={ baseStyles.link }
+              href="mailto:hello@colinmeinke.com"
+            >
+              hello@colinmeinke.com
+            </a>
+          </p>
+        </section>
       </section>
     </section>
   </DocumentTitle>
