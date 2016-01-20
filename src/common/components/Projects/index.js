cdb646a7940a7970344bef19d28fa928930b77fd
Colin Meinke <hello@colinmeinke.com>
2016-01-20 09:57:33 -0600

    refactor(copy): expand on project list

--- src/common/components/Projects/index.js
+++ src/common/components/Projects/index.js
@@ -8,27 +8,36 @@ import baseStyles from './base.css';
 
 const projects = [
   {
-    name: 'UniversalJS starter-kit',
+    description: `A universal Javascript starter kit inc. React, Redux, Redux
+                  Dev Tools, Universal Redux Router, CSS Modules, hot module
+                  reloading, Babel for ES2015+ and ESLint.`,
+    name: 'UniversalJS',
     url: 'https://github.com/colinmeinke/universal-js',
   },
   {
+    description: `A very simple router for Redux and React that works on both
+                  server and client.`,
     name: 'Universal Redux Router',
     url: 'https://github.com/colinmeinke/universal-redux-router',
   },
   {
-    name: 'Kampot theme for Ghost',
+    description: 'A responsive theme for Ghost.',
+    name: 'Kampot',
     url: 'https://github.com/colinmeinke/kampot',
   },
   {
-    name: 'Oaxaca syntax theme for Atom',
+    description: 'A minimal syntax theme for Atom.',
+    name: 'Oaxaca syntax theme',
     url: 'https://github.com/colinmeinke/oaxaca-syntax-theme',
   },
   {
-    name: 'Custom props plugin for PostCSS',
+    description: 'A PostCSS plugin to resolve the CSS calc function.',
+    name: 'PostCSS custom props',
     url: 'https://github.com/colinmeinke/postcss-custom-props',
   },
   {
-    name: 'Calc function plugin for PostCSS',
+    description: 'A PostCSS plugin to enable CSS Custom Properties (CSS variables).',
+    name: 'PostCSS calc function',
     url: 'https://github.com/colinmeinke/postcss-calc-function',
   },
 ];
@@ -41,14 +50,25 @@ const Projects = () => (
       <section className={ baseStyles.containter }>
         <h2 className={ baseStyles.title }>Projects</h2>
 
-        <ul>
+        <ul className={ baseStyles.projectList }>
           { projects.map(( project, i ) => (
-            <li key={ i }>
+            <li
+              className={ baseStyles.projectItem }
+              key={ i }
+            >
+              <h3 className={ baseStyles.projectTitle }>
+                { project.name }
+              </h3>
+
+              <p className={ baseStyles.projectDescription }>
+                { project.description }
+              </p>
+
               <a
-                className={ baseStyles.link }
+                className={ baseStyles.projectLink }
                 href={ project.url }
               >
-                { project.name }
+                View on github
               </a>
             </li>
           ))}
