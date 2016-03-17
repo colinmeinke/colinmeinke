232e72839db1bd00b4286ee203426a6f6a4fd412
Colin Meinke <hello@colinmeinke.com>
2016-03-17 13:07:35 -0600

    refactor(status): update activity

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -77,7 +77,7 @@ class Header extends Component {
           <p className={ activityLocation }>
             Currently { ' ' }
             <strong className={ activity }>
-              learning too much about bezier curves
+              drawing
             </strong>
             { ' ' } in { ' ' }
             <strong className={ location }>
