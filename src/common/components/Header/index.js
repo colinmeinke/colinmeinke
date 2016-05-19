dbbedc67bb2e1a6c5725852912cab1169411b420
Colin Meinke <hello@colinmeinke.com>
2016-05-18 19:43:47 -0500

    refactor(status): update activity

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -77,7 +77,7 @@ class Header extends Component {
           <p className={ activityLocation }>
             Currently { ' ' }
             <strong className={ activity }>
-              making SVG tools
+              at war with the waves
             </strong>
             { ' ' } in { ' ' }
             <strong className={ location }>
