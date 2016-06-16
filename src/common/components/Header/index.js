b94690641e3bd3bbf79c9d1365283956d2634488
Colin Meinke <hello@colinmeinke.com>
2016-06-16 08:19:56 +0100

    refactor(status): update activity

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -77,7 +77,7 @@ class Header extends Component {
           <p className={ activityLocation }>
             Currently { ' ' }
             <strong className={ activity }>
-              at home
+              at a birthday party
             </strong>
             { ' ' } in { ' ' }
             <strong className={ location }>
