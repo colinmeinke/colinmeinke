03bd57d0c1f19a496d978f3529fddf56054f4045
Colin Meinke <hello@colinmeinke.com>
2016-09-20 18:24:18 +0200

    test: remove env/url helper tests and add rootUrl tests

---
+++ test/actions/rootUrl.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+
+import { UPDATE_ROOT_URL, updateRootUrl } from '../../src/common/actions/rootUrl';
+
+describe( 'action creator', () => {
+  describe( 'rootUrl', () => {
+    describe( 'updateRootUrl()', () => {
+      it( 'should create an action to update rootUrl', () => {
+        const protocol = 'https';
+        const host = 'www.colinmeinke.com';
+
+        const expectedAction = {
+          type: UPDATE_ROOT_URL,
+          url: `${ protocol }://${ host }`,
+        };
+
+        expect( updateRootUrl( protocol, host )).toEqual( expectedAction );
+      });
+    });
+  });
+});
