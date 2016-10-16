fe5a9bd1b1b1d13fa8aad91cd71125141fbebad0
Colin Meinke <hello@colinmeinke.com>
2016-10-16 21:00:17 +0200

    refactor(status): update location

--- src/common/components/Header/index.js
+++ src/common/components/Header/index.js
@@ -81,7 +81,7 @@ class Header extends Component {
             </strong>
             { ' ' } in { ' ' }
             <strong className={ location }>
-              Ljubljana, Slovenia
+              Zagreb, Croatia
             </strong>.
           </p>
         </div>
